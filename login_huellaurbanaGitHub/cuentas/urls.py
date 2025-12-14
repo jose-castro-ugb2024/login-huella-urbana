728e4fe4a809e9e67c8d7d420142fab8b80cf04e
Jose Castro <ustr005624@ugb.edu.sv>
2025-12-14 07:50:26 -0600

    Update urls.py

--- login_huellaurbanaGitHub/cuentas/urls.py
+++ login_huellaurbanaGitHub/cuentas/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.login_usuario, name='inicio'),  # Raíz va al login
     path('login/', views.login_usuario, name='login'),
     path('registro/', views.registro_usuario, name='registro'),
+    path('pagina/<int:pk>/', views.pagina_detalle, name='pagina_detalle')
 ]
