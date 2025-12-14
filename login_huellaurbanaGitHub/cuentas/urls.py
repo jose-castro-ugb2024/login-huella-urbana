f8025aa5ae75d1e1cb52c7bccb9b4b2d51cf4f90
Jose Castro <ustr005624@ugb.edu.sv>
2025-12-13 21:03:49 -0600

    Update urls.py

--- login_huellaurbanaGitHub/cuentas/urls.py
+++ login_huellaurbanaGitHub/cuentas/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.login_usuario, name='inicio'),  # Raíz va al login
     path('login/', views.login_usuario, name='login'),
     path('registro/', views.registro_usuario, name='registro'),
 ]
